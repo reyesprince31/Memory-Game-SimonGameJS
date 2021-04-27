759856c802707c779a3afd581503efdf7bed9766
Prince Reyes <krayzerprince@gmail.com>
2021-04-27 22:31:35 +0800

    start the game and check answer

--- game.js
+++ game.js
@@ -1,35 +1,65 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
-const userClickedPattern = [];
+let userClickedPattern = [];
+let started = false;
+let level = 0;
+
+document.addEventListener("keydown", () => {
+	// console.log(e.key);
+	if (!started) {
+		nextSequence();
+		document.querySelector("#level__title").innerHTML = `Level ${level}`;
+		started = true;
+	}
+});
 
 const nextSequence = () => {
+	userClickedPattern = [];
 	const randomNumber = Math.round(Math.random() * 3);
 	const randomChosenColor = buttonColors[randomNumber];
 
-	console.log(randomChosenColor);
-
+	// console.log(randomChosenColor);
 	gamePattern.push(randomChosenColor);
 
-	console.log(gamePattern);
+	console.log("Random pattern: " + gamePattern);
 
 	animatePress(randomChosenColor, 0);
 	playSound(randomChosenColor);
+
+	level++;
+	//document.querySelector("#level__title").innerHTML = `Level ${level}`;
 };
 
 const buttonSelector = document.querySelectorAll(".btn");
-
+//console.log(buttonSelector);
 for (let i = 0; i < buttonSelector.length; i++) {
 	document.querySelectorAll(".btn")[i].addEventListener("click", (e) => {
 		const userChosenColor = e.target.value;
-		console.log(userChosenColor);
-
+		// console.log(userChosenColor);
 		userClickedPattern.push(userChosenColor);
-		console.log(userClickedPattern);
+		console.log("User Click: " + userClickedPattern);
 
 		animatePress(userChosenColor, 1);
 		playSound(userChosenColor);
+
+		checkAnswer(userClickedPattern.length - 1);
 	});
 }
+
+const checkAnswer = (currentLevel) => {
+	if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+		console.log("success");
+
+		if (userClickedPattern.length === gamePattern.length) {
+			console.log("ahhhhhhh");
+			setTimeout(function () {
+				nextSequence();
+			}, 1000);
+		}
+	} else {
+		console.log("wrong");
+	}
+};
 
 const animatePress = (currentColor, num) => {
 	const elements = document.querySelectorAll(`.box__${currentColor}`);
