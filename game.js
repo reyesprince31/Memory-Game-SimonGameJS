6401dfd614711f0ff2c948e7e662e4cce9c3793c
Prince Reyes <krayzerprince@gmail.com>
2021-04-27 21:16:06 +0800

    select button that cliked

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
+const userClickedPattern = [];
 
 const nextSequence = () => {
 	const randomNumber = Math.round(Math.random() * 3);
@@ -11,3 +12,15 @@ const nextSequence = () => {
 
 	console.log(gamePattern);
 };
+
+const buttonSelector = document.querySelectorAll(".btn");
+
+for (let i = 0; i < buttonSelector.length; i++) {
+	document.querySelectorAll(".btn")[i].addEventListener("click", (e) => {
+		const userChosenColor = e.target.value;
+		console.log(userChosenColor);
+
+		userClickedPattern.push(userChosenColor);
+		console.log(userClickedPattern);
+	});
+}
